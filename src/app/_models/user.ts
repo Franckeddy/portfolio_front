1332da02f9d1dc536aeedc9e46df4247ec0f4cf9
Franck garcon <franck.garcon@gmail.com>
2019-10-23 00:35:54 +0200

    [WIP]make models->interface

--- src/app/_models/user.ts
+++ src/app/_models/user.ts
@@ -1,8 +1,12 @@
 import { EmailValidator } from '@angular/forms';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 // Le modèle utilisateur est une petite classe qui définit les propriétés d'un utilisateur. 
 // La propriété token est utilisée pour contenir le jeton JWT renvoyé par l'API en cas d'authentification réussie.
 
-export class User {
+export interface User {
     id: number;
     username: { type: String, required: true };
     password: { type: String, required: true };
@@ -11,4 +15,15 @@ export class User {
     token: string;
     email: { type: EmailValidator, required: true };
     roles: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+  private usersUrl = 'api/users';
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.usersUrl);
+  }
 }

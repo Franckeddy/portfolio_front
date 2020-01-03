9fdd99f2b21b42cab7bc937eaf50054f72f9636d
Franck garcon <franck.garcon@gmail.com>
2020-01-03 14:46:10 +0100

    register fields

--- src/app/components/register/register/register.component.ts
+++ src/app/components/register/register/register.component.ts
@@ -36,8 +36,8 @@ export class RegisterComponent implements OnInit {
 
     ngOnInit() {
         this.registerForm = this.formBuilder.group({
-            // firstName: ['', Validators.required],
-            // lastName: ['', Validators.required],
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
             username: ['', Validators.required],
             password: ['', [Validators.required, Validators.minLength(6)]]
         });
